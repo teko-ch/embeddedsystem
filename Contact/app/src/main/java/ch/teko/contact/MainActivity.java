367a467182fdb77a396537ed2148511c81df64f3
Jonathan Brutschin <j.brutschin@gmail.com>
2017-06-12 13:34:36 +0200

    Contact App

---
+++ Contact/app/src/main/java/ch/teko/contact/MainActivity.java
@@ -0,0 +1,76 @@
+package ch.teko.contact;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = this.getClass().getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Define AuthState Listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if(user == null){
+                    Log.d(TAG, "Nicht authentifizierter Benutzer");
+                    startActivity(new Intent(getBaseContext(), SignInActivity.class));
+                }else{
+                    Log.d(TAG, "Current User: " + user.getEmail());
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.default_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                mAuth.signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
---
+++ Contact/app/src/main/java/ch/teko/contact/SignInActivity.java
@@ -0,0 +1,71 @@
+package ch.teko.contact;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    protected String TAG = this.getClass().getSimpleName();
+
+    protected FirebaseAuth mAuth;
+
+    /*
+     * Views
+     */
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // View
+        mEmailField = (EditText) findViewById(R.id.login_fld_email);
+        mPasswordField = (EditText) findViewById(R.id.login_fld_password);
+
+        // Set listener to Login Button
+        findViewById(R.id.login_btn_login).setOnClickListener(this);
+
+    }
+
+    private void signInWithMail(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()){
+                        Toast.makeText(SignInActivity.this, "Fehler", Toast.LENGTH_SHORT).show();
+                    }
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(getBaseContext(), MainActivity.class));
+                        finish();
+                    }
+                }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // Login Button
+        if(v.getId() == R.id.login_btn_login){
+            this.signInWithMail(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        }
+    }
+}
